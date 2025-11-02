4238f291acce4f67eb41ce3bc4a900e4126ae281
Seonwoo Choi <seonwoo31@dgu.ac.kr>
2025-11-02 20:28:09 +0900

    💄 Design: 헤더, 네브바 디자인 구현 및 theme font family 수정 (#7)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,47 @@
 import styled from 'styled-components';
 import { Link, useLocation } from 'react-router-dom';
+import { typography, c, s } from '../styles/themeUtils';
+
+import home from '../assets/icons/NavHome.svg';
+import photo from '../assets/icons/NavPhoto.svg';
+import chat from '../assets/icons/NavChat.svg';
+import game from '../assets/icons/NavGame.svg';
+import my from '../assets/icons/NavMy.svg';
 
 const Nav = styled.nav`
-  background: ${({ theme }) => theme.colors.white};
-  border-top: 1px solid ${({ theme }) => theme.colors.gray300};
+  background: ${c('neutral.black')};
+  height: 60px;
   display: flex;
   justify-content: space-around;
   align-items: center;
-  padding: ${({ theme }) => theme.spacing.sm} 0;
+  padding: 12px 11px;
 `;
 
 const NavItem = styled(Link)`
-  color: ${({ $active, theme }) => ($active ? theme.colors.primary : theme.colors.gray900)};
-  text-decoration: none;
-  font-size: 14px;
-  font-weight: 500;
+  ${typography('caption01')};
+  color: ${({ $active, theme }) => ($active ? theme.colors.brand.pink : theme.colors.neutral.bg)};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 40px;
+  width: 30px;
+`;
+
+const ChatIcon = styled(NavItem)`
+  flex-basis: 30px;
+  background: ${c('brand.pink')};
+  border: 5px solid ${c('neutral.black')};
+  border-radius: 99px;
+  padding: 19px;
+  transform: translateY(-11px);
+  width: 70px;
+  height: 70px;
+  justify-content: center;
+
+  img {
+    width: 32px;
+    height: 32px;
+  }
 `;
 
 export default function Navbar() {
@@ -23,18 +50,22 @@ export default function Navbar() {
   return (
     <Nav>
       <NavItem to="/" $active={pathname === '/'}>
+        <img src={home} alt="HOME" />
         HOME
       </NavItem>
       <NavItem to="/photo" $active={pathname === '/photo'}>
+        <img src={photo} alt="PHOTO" />
         PHOTO
       </NavItem>
-      <NavItem to="/chat" $active={pathname === '/chat'}>
-        CHAT
-      </NavItem>
+      <ChatIcon to="/chat" $active={pathname === '/chat'}>
+        <img src={chat} alt="CHAT" />
+      </ChatIcon>
       <NavItem to="/game" $active={pathname === '/game'}>
+        <img src={game} alt="GAME" />
         GAME
       </NavItem>
       <NavItem to="/mypage" $active={pathname === '/mypage'}>
+        <img src={my} alt="MY" />
         MY
       </NavItem>
     </Nav>

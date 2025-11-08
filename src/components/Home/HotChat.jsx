10403d66454f4f810e6655d96cf120aa7b175c65
Seonwoo Choi <seonwoo31@dgu.ac.kr>
2025-11-08 15:57:18 +0900

    ✨ Feat: 인기있는 이야기 카드 컴포넌트 생성

---
+++ src/components/Home/HotChat.jsx
@@ -0,0 +1,67 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { c, typography } from '../../styles/themeUtils';
+import { BoxContainer } from './BoxContainer';
+import defaultProfile from '../../assets/icons/profile/avatar1.svg';
+
+export default function HotChat({
+  image = defaultProfile,
+  nickname = '행복한 눈멍이',
+  time = '4분 전',
+  content = '닭갈비 맛있어요',
+}) {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <Profile>
+        <img
+          src={defaultProfile}
+          alt="프로필 이미지"
+          onClick={() => navigate('/chat')}
+          style={{ cursor: 'pointer' }}
+          width="40px"
+        />
+        <ProfileWrapper>
+          <Nickname>{nickname}</Nickname>
+          <Time>{time}</Time>
+        </ProfileWrapper>
+      </Profile>
+      <Content>{content}</Content>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  background: ${c('neutral.bg')};
+  padding: 16px;
+  border-radius: 12px;
+  gap: 16px;
+  box-shadow:
+    0 1px 3px 0 rgba(0, 0, 0, 0.12),
+    0 4px 8px 0 rgba(0, 0, 0, 0.08);
+`;
+
+const Profile = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 12px;
+`;
+
+const ProfileWrapper = styled.div`
+  gap: 4px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Nickname = styled.span`
+  ${typography('label01')};
+`;
+
+const Time = styled.span`
+  ${typography('caption01')};
+`;
+const Content = styled.div`
+  ${typography('body01')};
+`;

7216ed8c949cfbe148e9c544c3e3f86d18260ceb
Seonwoo Choi <seonwoo31@dgu.ac.kr>
2025-11-11 02:05:58 +0900

    ✨ Feat: 게임 진입 페이지 피그마 수정에 따른 수정 (#79)

--- src/components/Game/Alert.jsx
+++ src/components/Game/Alert.jsx
@@ -1,14 +1,25 @@
-import alert from '../../assets/icons/GameAlert.svg';
 import styled from 'styled-components';
 import { c, f, s, typography } from '../../styles/themeUtils';
-export default function Alert() {
+import down from '../../assets/icons/GameArrowDown.svg';
+export default function Alert({ icon, title, text, text2, text3, alerttext }) {
   return (
-    <>
-      <Container>
-        <img src={alert} />
-        <Content>안녕</Content>
-      </Container>
-    </>
+    <Container>
+      <DropdownWrapper>
+        <TitleWrapper>
+          {icon && <img src={icon} alt="alert icon" />} {/* 아이콘 있을 때만 */}
+          {title && <Title>{title}</Title>} {/* 제목 있을 때만 */}
+          {alerttext && <Content>{alerttext}</Content>} {/* 알럿텍스트 있을 때만 */}
+        </TitleWrapper>
+        {(text || text2) && <Dropdown src={down} />}
+      </DropdownWrapper>
+      {(text || text2) /* text가 하나라도 있으면 출력 */ && (
+        <ContentWrapper>
+          {text && <Content>{text}</Content>}
+          {text2 && <Content>{text2}</Content>}
+          {text3 && <Content>{text3}</Content>}
+        </ContentWrapper>
+      )}
+    </Container>
   );
 }
 const Container = styled.div`
@@ -16,10 +27,33 @@ const Container = styled.div`
   width: 100%;
   padding: 10px 16px;
   display: flex;
+  gap: 20px;
+  flex-direction: column;
+  background-color: ${c('neutral.gray')};
+`;
+const TitleWrapper = styled.div`
+  display: flex;
   gap: 11px;
   flex-direction: row;
-  background-color: ${c('neutral.gray')};
+`;
+const Title = styled.h1`
+  ${typography('label01')};
 `;
 const Content = styled.div`
   ${typography('body01')};
+`;
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding-left: 12px;
+`;
+const Dropdown = styled.img`
+  width: 24px;
+  height: 24px;
+`;
+const DropdownWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;

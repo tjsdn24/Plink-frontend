493d8eb8536c7ed6ecf5f6c38c7c826e38dcba79
Seonwoo Choi <seonwoo31@dgu.ac.kr>
2025-11-14 15:21:54 +0900

    Feat: 사진 보이게 수정

--- src/components/Home/HotChat.jsx
+++ src/components/Home/HotChat.jsx
@@ -3,35 +3,59 @@ import styled from 'styled-components';
 import { c, typography } from '../../styles/themeUtils';
 import { BoxContainer } from './BoxContainer';
 import defaultProfile from '../../assets/icons/profile/avatar1.svg';
-
+import dots from '../../assets/icons/HomeThree.svg';
 export default function HotChat({
+  id,
+  postList = [],
   image = defaultProfile,
   nickname = '행복한 눈멍이',
   time = '4분 전',
   content = '닭갈비 맛있어요',
+  images = [],
 }) {
   const navigate = useNavigate();
 
+  const handleClick = () => {
+    const targetPost = postList.find(p => p.id === id);
+
+    navigate(`/chat/post/${id}`, {
+      state: { post: targetPost },
+    });
+  };
+
   return (
     <Container>
-      <Profile>
-        <img
-          src={image || defaultProfile}
-          alt="프로필 이미지"
-          onClick={() => navigate('/chat')}
-          style={{ cursor: 'pointer' }}
-          width="40px"
-        />
-        <ProfileWrapper>
-          <Nickname>{nickname}</Nickname>
-          <Time>{time}</Time>
-        </ProfileWrapper>
-      </Profile>
-      <Content>{content}</Content>
+      <Wrapper>
+        <Profile>
+          <img src={image} alt="프로필 이미지" width="40px" />
+          <ProfileWrapper>
+            <Nickname>{nickname}</Nickname>
+            <Time>{time}</Time>
+          </ProfileWrapper>
+        </Profile>
+      </Wrapper>
+
+      <Content onClick={handleClick} style={{ cursor: 'pointer' }}>
+        {content}
+        {/* images[0] 있을 때만 미리보기 이미지 렌더링 */}
+
+        {images.length > 0 && <PreviewImage src={images[0].imageUrl} alt="미리보기 이미지" />}
+      </Content>
     </Container>
   );
 }
+const PreviewImage = styled.img`
+  width: 100%;
+  border-radius: 8px;
+  margin-top: 12px;
+  cursor: pointer;
+`;
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 const Container = styled.div`
   background: ${c('neutral.bg')};
   padding: 16px;
